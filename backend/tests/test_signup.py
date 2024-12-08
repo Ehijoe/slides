65c307c2eec2426f6a4120e8ffcb2a9ba9457208
Boluwatife <boluwatifelekeoduoye@gmail.com>
2024-12-08 18:50:39 +0100

    created tests folder and finished implementing sign[Fup_create_user test

---
+++ backend/tests/test_signup.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+client = TestClient(app)
+
+
+def test_signup_create_user():
+    response = client.post(
+        "/auth/signup/",
+        json={"name": "Bazz", "email": "bazz@gmail.com" , "password": "Dropthebazz", "password_confirm": "Dropthebazz"},
+    )
+    assert response.status_code == 200
+    assert response.json()["name"]
+    assert response.json()["email"]
+    assert response.json()["id"]
+    assert response.json()["is_active"] == False
